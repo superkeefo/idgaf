8363a5c9fefe405ebc3556922dd43583ad2cb52f
superkeefo <iamkeithtobin@gmail.com>
2025-05-31 13:44:46 +0100

    Refactor class structure: rename App to Control and update View initialization

--- idgaf.py
+++ idgaf.py
@@ -2,15 +2,24 @@ import os
 from os.path import join
 import customtkinter as ui
 
+class View(ui):
+    def __init__(self, control):
+        super().__init__()
+        self.control= control
+        pass
 
-class App:
+class Control: 
+    def __init__(self, view, model):
+        self.view = view
+        self.model = model
+        pass
+
+class Model:
     def __init__(self):
         pass
 
-class FileHandling:
-    def __init__(self):
-        pass
-
-class FfmpegPackage:
-    def __init__(self):
-        pass
+    
+model = Model()
+view = View(None)
+control = Control(view,model)
+view.control = control
