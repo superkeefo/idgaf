7eb7758edf3837ba426e33513aa5343472a502e4
superkeefo <iamkeithtobin@gmail.com>
2025-05-31 12:16:10 +0100

    Add .gitignore and initial implementation of core classes

---
+++ idgaf.py
@@ -0,0 +1,16 @@
+import os
+from os.path import join
+import customtkinter as ui
+
+
+class App:
+    def __init__(self):
+        pass
+
+class FileHandling:
+    def __init__(self):
+        pass
+
+class FfmpegPackage:
+    def __init__(self):
+        pass
