8904540b31331bfca6934face205fffaa07518b3
superkeefo <iamkeithtobin@gmail.com>
2025-06-02 21:20:10 +0100

    Refactor Model and View classes: enhance GIF settings, improve UI layout, and add input/output handling

--- idgaf.py
+++ idgaf.py
@@ -1,5 +1,4 @@
 import os
-from os.path import join
 import customtkinter as ui
 
 class Model:
@@ -13,27 +12,30 @@ class Model:
         self.prefix = "Howabout"
 
         ### GIF SETTING OPTIONS ###
-        stats_mode = ['full','diff']              # 0,1
-        diff_mode = ['rectangle','none']          # 0,1
-        colours = [32,64,128,256]                 # 0,1,2,3
-        dither_method = ['bayer:bayer_scale=1',   #0 high quality bayer
-                         'bayer:bayer_scale=2',   #1
-                         'bayer:bayer_scale=3',   #2
-                         'bayer:bayer_scale=4',   #3
-                         'bayer:bayer_scale=5',   #4 low quality bayer
-                         'sierra2',               #5
-                         'floyd_steinberg',       #6
-                         'sierra2_4a',            #7 Highest size
-                         'none']                  #8 ugly
+        self.loop_list = ['Yes','No']
+        self.optimize_list = ['Animation','Video']
+        self.scale_list = ['100%','75%','50%','25%']
+        self.stats_mode_list = ['full','diff']              # 0,1
+        self.diff_mode_list = ['rectangle','none']          # 0,1
+        self.colours_list = [256, 192, 128, 96, 64, 48,32]  # 0,1,2,3
+        self.dither_method_list = ['bayer:bayer_scale=1',   # 0 high quality bayer
+                                   'bayer:bayer_scale=2',   # 1
+                                   'bayer:bayer_scale=3',   # 2
+                                   'bayer:bayer_scale=4',   # 3
+                                   'bayer:bayer_scale=5',   # 4 low quality bayer
+                                   'sierra2',               # 5
+                                   'floyd_steinberg',       # 6
+                                   'sierra2_4a',            # 7 Highest size
+                                   'none']                  # 8 ugly
         
 
         ### GIF SETTINGS ###
         self.fps = 8
         self.scalediv = 2
-        self.max_colours = colours[1]
-        self.dither_method = dither_method[8]
-        self.stats_mode = stats_mode[1]
-        self.diff_mode = diff_mode[0]
+        self.max_colours = self.colours_list[1]
+        self.dither_method = self.dither_method_list[8]
+        self.stats_mode = self.stats_mode_list[1] #testing this one
+        self.diff_mode = self.diff_mode_list[0]
         self.loops = 0
 
         ### OUTPUT NAME ###
@@ -119,28 +121,118 @@ class View(ui.CTk): # display and pass to controller only
         self.control = control
         ui.set_appearance_mode("dark")
         ui.set_default_color_theme("green")
-        self.geometry("260x250")
+        self.geometry("300x700")
         self.title("I don't gif a f***")
         # self.iconbitmap(os.path.join('icons','punch.ico'))
         self.resizable(False, False)
-        self.find_btn = self.btn(200, 50, "find file", 30, 30, 
-                                 self.control.find_file_location)
-        self.save_btn = self.btn(200, 50, "Set save Location", 30, 100, 
-                                 self.control.set_save_location)
-        self.gen_btn = self.btn(200, 50, "Generate gif", 30, 170, 
-                                self.control.generate_gif)
+
+
+
+        # fps
+        self.fps_area = self.area(300,50,0,10,self)
+        self.fps_text  = self.menu_text("FPS:",20,12,self.fps_area)
+        self.fps_input = self.input(125,35,155,7.5,self.fps_area)
+        self.fps_input.insert(0,'30') # will need to change this when preferences are saved/called
+
+        # scale
+        self.scale_area = self.area(300,50,0,60,self)
+        self.scale_text  = self.menu_text("Scale:",20,12,self.scale_area)
+        self.scale_dd = self.drop_down(125,35,155,7.5, self.control.model.scale_list, self.scale_area)
+
+        # max colours
+        self.max_area = self.area(300,50,0,110,self)
+        self.max_text  = self.menu_text("Max colours:",20,12,self.max_area)
+        self.max_dd = self.drop_down(125,35,155,7.5, self.control.model.colours_list, self.max_area)
+
+        # dither method
+        self.dith_area = self.area(300,50,0,160,self)
+        self.dith_text  = self.menu_text("Dither method:",20,12,self.dith_area)
+        self.dith_dd = self.drop_down(125,35,155,7.5, self.control.model.dither_method_list, self.dith_area)
+
+        # optimize preference
+        self.opt_area = self.area(300,50,0,210,self)
+        self.opt_text  = self.menu_text("Optimize for:",20,12,self.opt_area)
+        self.opt_dd = self.drop_down(125,35,155,7.5, self.control.model.optimize_list, self.opt_area)
+
+        # loop preference
+        self.loop_area = self.area(300,50,0,260,self)
+        self.loop_text  = self.menu_text("Loop:",20,12,self.loop_area)
+        self.loop_dd = self.drop_down(125,35,155,7.5, self.control.model.loop_list, self.loop_area)
+
+        # file input
+        self.input_text = self.menu_text("Select input:",20,320,self)
+        self.input_text.configure(font = ('Roboto Bold', 16))
+        self.input_area = self.area(300, 60, 0, 350, self)
+        self.single_file_btn = self.btn(125,40,"Single File",20,10, self.control.find_file_location ,self.input_area)
+        self.folder_btn = self.btn(125,40,"Folder",155,10,None,self.input_area)
+
+        # Set output
+        self.output_text = self.menu_text("Select save location:",20,415,self)
+        self.output_text.configure(font = ('Roboto Bold', 16))
+        self.output_area = self.area(300, 500, 0, 435, self)
+        self.output_input = self.input(260, 40, 20, 10, self.output_area)
+        self.output_btn = self.btn(260, 40, "Set Output Folder", 20, 60, self.control.set_save_location, self.output_area)
+        self.makegif_btn = self.btn(260, 80, "Generate Gif(s)!", 20, 125, self.control.generate_gif, self.output_area)
+        
+        # self.find_btn = self.btn(200, 50, "find file", 30, 30, 
+        #                          self.control.find_file_location, self)
+        # self.save_btn = self.btn(200, 50, "Set save Location", 30, 100, 
+        #                          self.control.set_save_location, self)
+        # self.gen_btn = self.btn(200, 50, "Generate gif", 30, 170, 
+        #                         self.control.generate_gif, self)
         
 
-    def btn(self, set_width, set_height, set_text, set_x, set_y,set_command,):
-        button = ui.CTkButton(self, 
+    def btn(self, set_width, set_height, set_text, set_x, set_y,set_command,set_root):
+        button = ui.CTkButton(set_root, 
                               width=set_width, 
                               height= set_height, 
                               text=set_text, 
                               text_color='#000000',
+                              font=("Roboto Bold", 16),
                               command=set_command)
         button.place(x=set_x, y=set_y)
         return button
-        
+    
+    def area(self, set_width, set_height, set_x, set_y, set_root):
+        label = ui.CTkLabel(set_root,
+                            width=set_width,
+                            height=set_height,
+                            text=None,
+                            bg_color='transparent')
+        label.place(x=set_x,y=set_y)
+        return label
+    
+    def input(self, set_width, set_height, set_x, set_y,set_root):
+        input = ui.CTkEntry(set_root, 
+                             width=set_width, 
+                             height= set_height,
+                             font=("Roboto Bold", 16),
+                             justify="center",
+                             border_width=0)
+        input.place(x=set_x, y=set_y)
+        return input
+    
+    def menu_text(self, menu_text, set_x, set_y, set_root):
+        label = ui.CTkLabel(set_root,
+                            # width=set_width,
+                            # height=set_height,
+                            text=menu_text,
+                            font=("Roboto", 16))
+        label.place(x=set_x,y=set_y)
+        return label
+    
+    def drop_down(self, set_width, set_height, set_x, set_y, opt_list, set_root):
+        menu = ui.CTkOptionMenu(set_root, 
+                                 width = set_width, 
+                                 height = set_height,
+                                 values = [str(item) for item in opt_list],
+                                 font=("Roboto Bold", 16),
+                                 text_color="#000000",
+                                 dynamic_resizing=False)
+        menu.place(x=set_x, y=set_y)
+        return menu
+
+                    
 model = Model()
 control = Control(None, model) 
 view = View(control)             
